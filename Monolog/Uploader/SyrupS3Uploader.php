a819cffc330b8b72260445092cfc33835967b2bf
Miro Cillik <miro@keboola.com>
2013-02-22 12:11:00 +0100

    added missing files to repo

---
+++ Monolog/Uploader/SyrupS3Uploader.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * SyrupS3Uploader.php
+ *
+ * @author: Miroslav Čillík <miro@keboola.com>
+ * @created: 22.1.13
+ */
+
+namespace Syrup\ComponentBundle\Monolog\Uploader;
+
+use Aws\S3\S3Client;
+use Aws\Common\Aws;
+use Aws\Common\Enum\Region;
+
+class SyrupS3Uploader
+{
+	/**
+	 * @var array aws-access-key, aws-secret-key, s3-upload-path, bitly-login, bitly-api-key
+	 */
+	protected $_config;
+
+	/**
+	 * @var S3Client
+	 */
+	protected $_s3;
+
+	public function __construct($config)
+	{
+		$this->_config = $config;
+
+		$this->_s3 = $this->_getS3();
+	}
+
+	public function uploadString($name, $content, $contentType = 'text/plain')
+	{
+		$s3FileName = $this->_fileUniquePrefix() . $name;
+		$s3Path = $this->_config['s3-upload-path'] . '/' . $s3FileName;
+
+		$this->_s3->putObject($s3Path, array(
+			'Bucket' => $this->_config['s3-upload-path'],
+			'Key'    => $s3FileName,
+			'Body'   => $content,
+			'ACL'    => CannedAcl::PUBLIC_READ,
+			'ContentType'   => $contentType
+		));
+		$url = 'https://s3.amazonaws.com/' . $s3Path;
+
+		try {
+			return $this->_shortenUrl($url);
+		} catch (\Exception $e) {
+			return $url;
+		}
+	}
+
+	protected function _fileUniquePrefix()
+	{
+		return date('Y/m/') . date('Y-m-d-H-i-s') . '-' . uniqid() . '-';
+	}
+
+	protected function _shortenUrl($url)
+	{
+		$client = new \Guzzle\Http\Client(
+			'http://api.bitly.com/v3',
+			array(
+				'request.params' => array(
+					'login'     => $this->_config['bitly-login'],
+					'apiKey'    => $this->_config['bitly-api-key'],
+					'longUrl'   => $url,
+					'format'    => 'json'
+				)
+			)
+		);
+		$request = $client->get('shorten');
+		$response = $request->send();
+
+		$body = $response->json();
+
+		if (!isset($body['data']) || !isset($body['data']['url'])) {
+			throw new \Exception('Bit.ly url not returned');
+		}
+
+		return $body['data']['url'];
+	}
+
+	/**
+	 * @return S3Client
+	 */
+	protected function _getS3()
+	{
+		$s3 = S3Client::factory(array(
+			$this->_config['aws-access-key'],
+			$this->_config['aws-secret-key']
+		));
+
+		return $s3;
+	}
+
+
+}
---
+++ Tests/Monolog/SyrupS3UploaderTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * SyrupS3UploaderTest.php
+ *
+ * @author: Miroslav Čillík <miro@keboola.com>
+ * @created: 23.1.13
+ */
+class SyrupS3UploaderTest
+{
+
+}
