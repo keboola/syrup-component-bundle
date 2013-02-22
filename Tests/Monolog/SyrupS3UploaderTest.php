1e5cf702ff4bd302c723c4b544b88fecc4505d22
Miro Cillik <miro@keboola.com>
2013-02-22 13:46:59 +0100

    s3 upload tests

--- Tests/Monolog/SyrupS3UploaderTest.php
+++ Tests/Monolog/SyrupS3UploaderTest.php
@@ -5,7 +5,23 @@
  * @author: Miroslav Čillík <miro@keboola.com>
  * @created: 23.1.13
  */
-class SyrupS3UploaderTest
+use Syrup\ComponentBundle\Monolog\Uploader\SyrupS3Uploader;
+
+class SyrupS3UploaderTest extends \PHPUnit_Framework_TestCase
 {
+	public function testMonologUploader()
+	{
+		$uploader = new SyrupS3Uploader(array(
+			"aws-access-key" => "AKIAJJKM4R26QUNBFPTA",
+	        "aws-secret-key" => "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",
+	        "s3-upload-path" => "keboola-logs/debug-files",
+	        "bitly-login"    => "petrsimecek",
+	        "bitly-api-key"  => "R_35ed95e18188c58f7b42787f358857ea"
+		));
+
+		$url = $uploader->uploadString("This is a test error message - testing error uploading", "Body of the error message. bla bla blaaaaa.");
+
+		return $url;
+	}
 
 }
