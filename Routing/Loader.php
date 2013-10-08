98763d8c4b53c36ae06b5d5ad7ed97a8ad899ebe
Miro Cillik <miro@keboola.com>
2013-10-08 12:37:03 +0200

    Custom routing loader moved here from syrup

---
+++ Routing/Loader.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Loader.php
+ *
+ * @author: Miroslav Čillík <miro@keboola.com>
+ * @created: 26.9.13
+ */
+
+namespace Syrup\ComponentBundle\Routing;
+
+use Symfony\Component\Config\Loader\Loader as BaseLoader;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollection;
+
+class Loader extends BaseLoader
+{
+	/** @var array */
+	protected $components;
+
+	public function __construct($components)
+	{
+		$this->components = $components;
+	}
+
+	/**
+	 * Loads a resource.
+	 *
+	 * @param mixed $resource The resource
+	 * @param string $type     The resource type
+	 * @return \Symfony\Component\Routing\RouteCollection
+	 */
+	public function load($resource, $type = null)
+	{
+		$collection = new RouteCollection();
+
+		foreach ($this->components as $component) {
+
+			if (!isset($component['bundle'])) {
+				continue;
+			}
+
+			$bundleClassName = preg_replace('/^.*\\\/', '', $component['bundle']);
+			$resource = '@' . $bundleClassName . '/Resources/config/routing.yml';
+			$type = 'yaml';
+
+			$importedRoutes = $this->import($resource, $type);
+
+			$collection->addCollection($importedRoutes);
+		}
+
+		return $collection;
+	}
+
+	/**
+	 * Returns true if this class supports the given resource.
+	 *
+	 * @param mixed $resource A resource
+	 * @param string $type     The resource type
+	 *
+	 * @return Boolean true if this class supports the given resource, false otherwise
+	 */
+	public function supports($resource, $type = null)
+	{
+		return $type === 'component';
+	}
+}
