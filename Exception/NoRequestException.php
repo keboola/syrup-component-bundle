9b7247ec46573da6fe3b8ca499526415e111f9e4
Miro Cillik <miro@keboola.com>
2013-12-09 13:33:41 +0000

    StorageApi service refactored using Request in container

---
+++ Exception/NoRequestException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by Miroslav Čillík <miro@keboola.com>
+ * Date: 09/12/13
+ * Time: 11:56
+ */
+
+namespace Syrup\ComponentBundle\Exception;
+
+
+use Syrup\ComponentBundle\Exception\SyrupComponentException;
+
+class NoRequestException extends SyrupComponentException
+{
+
+	public function __construct()
+	{
+		parent::__construct(500, 'Request not set');
+	}
+
+} 
