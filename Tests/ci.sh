db7b0051e7627c8951919f4856ab3fc9eb2d8700
Miro Cillik <miro@keboola.com>
2014-02-25 15:36:50 +0000

    fix: travis and composer stuff

--- Tests/ci.sh
+++ Tests/ci.sh
@@ -2,12 +2,6 @@
 # export STORAGE_API_TOKEN=YOUR_TOKEN - run before this script
 
 cp parameters.yml.dist.tp parameters.yml.dist
-sed "s/SAPI_TOKEN/STORAGE_API_TOKEN/" -i "parameters.yml"
+sed "s/SAPI_TOKEN/$STORAGE_API_TOKEN/" -i "parameters.yml.dist"
 
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install -n
-php composer.phar update -n
-php composer.phar show --installed
-
-php vendor/bin/phpunit
 exit $?
