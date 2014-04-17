45e5c4207cb58141bf9346e973c8d4b699e09f6d
Martin Halamíček <martin@halamicek.cz>
2014-04-17 14:11:51 +0200

    tests: travis init fix

--- Tests/ci.sh
+++ Tests/ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # export STORAGE_API_TOKEN=YOUR_TOKEN - run before this script
 
-cp parameters.yml.dist.tp parameters.yml.dist
-sed "s/SAPI_TOKEN/$STORAGE_API_TOKEN/" -i "parameters.yml.dist"
+cp parameters.yml.dist parameters.yml
+sed "s/SAPI_TOKEN/$STORAGE_API_TOKEN/" -i "parameters.yml"
 
 exit $?
