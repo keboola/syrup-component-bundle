c555c00b1c708f0f510ad221e2bf7a77126a09f4
Jakub Matejka <jakub@keboola.com>
2015-02-12 07:34:47 +0000

    chore: deprecate FileSystem/Temp

--- Filesystem/Temp.php
+++ Filesystem/Temp.php
@@ -8,6 +8,7 @@ namespace Syrup\ComponentBundle\Filesystem;
 use Keboola\Temp\Temp as KeboolaTemp;
 
 /**
+ * @deprecated use Keboola\Temp\Temp instead (https://github.com/keboola/php-temp)
  * @brief A blank Temp class to provide backwards compatibility after separating a Temp class
  */
 class Temp extends KeboolaTemp
